9f1e774f978e0719fb68fb4fedcefdc8d73aaad7
bosha <boshaa3@gmail.com>
2018-12-03 17:21:18 +0200

    Трансформация данных API

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -8,37 +8,29 @@ export default class RandomPlanet extends Component {
 	swapiService = new SwapiService();
 
 	state = {
-		id: null,
-		name: null,
-		population: null,
-		rotationPeriod: null,
-		diameter: null
+		planet: {}
 	};
 
 	constructor() {
 		super();
+		this.onPlanetLoaded = this.onPlanetLoaded.bind(this);
 		this.updatePlanet();
 	}
 
+	onPlanetLoaded(planet) {
+		this.setState({ planet });
+	};
+
 	updatePlanet() {
-		const id = Math.floor(Math.random() * 25) + 2;
+		const id = Math.floor(Math.random() * (21 - 1) + 1);
 		this.swapiService
 			.getPlanet(id)
-			.then((planet) => {
-				console.log(planet);
-				this.setState({
-					id,
-					name: planet.name,
-					population: planet.population,
-					rotationPeriod: planet.rotation_period, //с сервера приходит не cC, а "_" формат
-					diameter: planet.diameter
-				});
-			});
+			.then(this.onPlanetLoaded);
 	}
 
 	render() {
 
-		const { id, name, population, rotationPeriod, diameter } = this.state;
+		const { planet: { id, name, population, rotationPeriod, diameter } } = this.state;
 
 		return (
 			<div className="random-planet jumbotron rounded">
