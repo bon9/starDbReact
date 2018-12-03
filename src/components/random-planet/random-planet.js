ef864eae899c55bbf4729e01f2cc95e64c0bba47
bosha <boshaa3@gmail.com>
2018-12-03 20:08:30 +0200

    Finished

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -37,8 +37,8 @@ export default class RandomPlanet extends Component {
 	};
 
 	updatePlanet() {
-		// const id = Math.floor(Math.random() * (21 - 1) + 1);
-		const id = 12000;
+		const id = Math.floor(Math.random() * (21 - 1) + 1);
+		// const id = 12000;
 		this.swapiService
 			.getPlanet(id)
 			.then(this.onPlanetLoaded)
