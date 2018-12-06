72f25f75d1c073eda48cfd8254e2a08d6b821910
bosha <boshaa3@gmail.com>
2018-12-06 20:36:47 +0200

    spinner not view

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -51,7 +51,7 @@ export default class App extends Component {
 						<ItemList onItemSelected={this.onPersonSelected} />
 					</div>
 					<div className="col-md-6">
-						<PersonDetails personID={this.state.selectedPerson} />
+						<PersonDetails personId={this.state.selectedPerson} />
 					</div>
 				</div>
 			</div >
