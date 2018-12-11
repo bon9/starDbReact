c3a482fa5367a46bfeb21d7e9735e78c7663d589
bosha <boshaa3@gmail.com>
2018-12-11 23:13:05 +0200

    render - функция

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 import ErrorIndicator from '../error-indicator/error-indicator';
+import SwapiService from '../../services/swapi-service';
 
 import './people-page.css';
 
+
 export default class PeoplePage extends Component {
+
+	swapiService = new SwapiService();
 
 	state = {
 		selectedPerson: null,
@@ -31,7 +35,11 @@ export default class PeoplePage extends Component {
 		return (
 			<div className="row mb2">
 				<div className="col-md-6">
-					<ItemList onItemSelected={this.onPersonSelected} />
+					<ItemList
+						onItemSelected={this.onPersonSelected}
+						getData={this.swapiService.getAllPeople}
+						renderItem={({ name, gender, birthYear }) =>
+							`${name} (${gender}, ${birthYear})`} />
 				</div>
 				<div className="col-md-6">
 					<PersonDetails personId={this.state.selectedPerson} />
