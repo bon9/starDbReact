d3317ca9e6111398fe1169d847ae50e34e868445
bosha <boshaa3@gmail.com>
2018-12-18 20:19:19 +0200

    Рефакторинг компонента высшего порядка

--- src/components/sw-components/starship-details.js
+++ src/components/sw-components/starship-details.js
@@ -21,4 +21,4 @@ const mapMethodsToProps = (swapiService) => {
 		getImageUrl: swapiService.getStarshipImage
 	};
 };
-export default withSwapiService(StarshipDetails, mapMethodsToProps);
+export default withSwapiService(mapMethodsToProps)(StarshipDetails);
