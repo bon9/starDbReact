526f80837c083e4d2ab87a0d67ebf2e089984531
bosha <boshaa3@gmail.com>
2018-12-25 21:06:20 +0200

    Updates

---
+++ precache-manifest.f03ab6a24290d151fab39fb87e6093c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "026194140a1a1d0299e6",
+    "url": "/bon9/starDbDeploy/static/js/runtime~main.02619414.js"
+  },
+  {
+    "revision": "2f2a6d01d44c4d28394c",
+    "url": "/bon9/starDbDeploy/static/js/main.2f2a6d01.chunk.js"
+  },
+  {
+    "revision": "d1710e1ba5d5a1199472",
+    "url": "/bon9/starDbDeploy/static/js/1.d1710e1b.chunk.js"
+  },
+  {
+    "revision": "2f2a6d01d44c4d28394c",
+    "url": "/bon9/starDbDeploy/static/css/main.8c9a903d.chunk.css"
+  },
+  {
+    "revision": "59a9d73fb6e7227d09c2a0da7d0256c4",
+    "url": "/bon9/starDbDeploy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/starDbDeploy/precache-manifest.c5e3a91eae0e72499634977809e7066b.js"
+  "/bon9/starDbDeploy/precache-manifest.f03ab6a24290d151fab39fb87e6093c8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/starDbDeploy/index.html", {
+workbox.routing.registerNavigationRoute("/bon9/starDbDeploy/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
