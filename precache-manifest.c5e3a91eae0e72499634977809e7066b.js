c8ec34dcaaf00baa978d317564de9272937c785c
bosha <boshaa3@gmail.com>
2018-12-25 21:03:26 +0200

    Updates

---
+++ precache-manifest.c5e3a91eae0e72499634977809e7066b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bf6d5f7db77c98a9831",
+    "url": "/starDbDeploy/static/js/runtime~main.1bf6d5f7.js"
+  },
+  {
+    "revision": "2f2a6d01d44c4d28394c",
+    "url": "/starDbDeploy/static/js/main.2f2a6d01.chunk.js"
+  },
+  {
+    "revision": "d1710e1ba5d5a1199472",
+    "url": "/starDbDeploy/static/js/1.d1710e1b.chunk.js"
+  },
+  {
+    "revision": "2f2a6d01d44c4d28394c",
+    "url": "/starDbDeploy/static/css/main.8c9a903d.chunk.css"
+  },
+  {
+    "revision": "c4aab2c3aabaed813c56c84696e93d1b",
+    "url": "/starDbDeploy/index.html"
+  }
+];
